c8336bd0c1e37f93224b5d76418b8200d2dc1c41
JoeMayinzi <joemayinzi@gmail.com>
2025-04-24 19:33:07 -0700

    (fixing) : module issues

---
+++ app/(overview)/policy/page.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
+
+export default function Page() {
+    return <DashboardLayout>
+        <div>Politique de confidentialité</div>
+    </DashboardLayout>
+  }
+  
